f2141fc101bb094338a8170b760d2b2d5e1527fb
Daniel Müller <daniel@dashwood.ch>
2025-04-25 07:41:21 +0200

    updated zsh deployment

--- linux-vm-defaults/defaults.sh
+++ linux-vm-defaults/defaults.sh
@@ -4,7 +4,9 @@ sudo apt install zsh git kitty-terminfo zsh-autosuggestions zsh-syntax-highlight
 cd /home/sysmin/ecmacom/linux-vm-defaults/
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 mkdir /home/sysmin/.config
-mv zsh /home/sysmin/.config/zsh
+cp zsh /home/sysmin/.config/zsh
+rm /home/sysmin/.zshrc
+rm /root/.zshrc
 ln -s /home/sysmin/.config/zsh/zshrc /home/sysmin/.zshrc
 sudo cp -r /home/sysmin/.oh-my-zsh /root/
 sudo cp /home/sysmin/.config/zsh/zshrc /root/.zshrc
